a0258149cbfaeaa065121216536863d893f6a391
elmira_k82 <elmirakirakosyan@gmail.com>
2018-09-10 19:20:08 +0400

    Do not allow print for Unknown in the home screen. Display the message box.

--- dashboard.php
+++ dashboard.php
@@ -557,7 +557,7 @@ $num_estimates = $qualify_requests;
 		    ?>
 			    <tr data-id="<?php //echo $estimate_request['Guid_brcaestimate']; ?>" class="t_row">
 				    <td class="printSelectBlock text-center">
-					<?php if($qualify_request['qualified']=='Unknown'){ ?>
+					<?php if(isset($qualify_request['qualified']) && $qualify_request['qualified']=='Unknown'){ ?>
 					    <input name="markedRow[user][<?php echo $qualify_request['Guid_user']; ?>]" type="checkbox" class="print1 report1" data-prinatble="0" data-selected_questionnaire="<?php echo $qualify_request['Guid_qualify']; ?>" data-selected_date="<?php echo $qualify_request['date']; ?>" />
 					<?php } else { ?>
 					    <input name="markedRow[user][<?php echo $qualify_request['Guid_user']; ?>]" type="checkbox" class="print1 report1" data-prinatble="1" data-selected_questionnaire="<?php echo $qualify_request['Guid_qualify']; ?>" data-selected_date="<?php echo $qualify_request['date']; ?>" />
