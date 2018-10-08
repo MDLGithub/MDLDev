96bf0eb5777a096d5652170e7d1fd8b7bfb5a57f
a-gokhale <42849787+a-gokhale@users.noreply.github.com>
2018-10-08 16:06:50 -0400

    Delete scripts.php

--- questionnaire/login/scripts.php
+++
@@ -1,6 +0,0 @@
-<script src="assets/js/admin.js"></script>
-<script src="assets/js/printThis.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="assets/libs/DataTables/datatables.min.js"></script>
-<script type="text/javascript" src="assets/libs/jquery.mask.js"></script>
-<script src="assets/libs/jquery-ui/jquery-ui.min.js"></script>
