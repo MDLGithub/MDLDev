77777d5b1f3fffa224fcf4ecda8255666172f8ad
a-gokhale <42849787+a-gokhale@users.noreply.github.com>
2018-10-09 11:57:03 -0400

    Add files via upload

---
+++ login/ajaxHandlerEvents.php
@@ -0,0 +1,89 @@
+<?php
+
+    // File Name: ajaxHandlerEvents.php
+
+?>
+
+<?php
+
+require_once('config.php');
+require_once ('functions_event.php');
+
+/* --------------------- Event Update ------------------------- */
+
+if(isset($_POST['modalhealthcareid']) && isset($_POST['action']) && $_POST['action'] == "eventupdate"){
+    $healthCare = array(
+         'name' => $_POST['full_name'],
+         'street1' => $_POST['street1'],
+         'street2' => $_POST['street2'],
+         'city' => $_POST['city'],
+         'state' => $_POST['state'],
+         'zip' => $_POST['zip'],
+     );
+     $where = array('Guid_healthcare' => $_POST['modalhealthcareid']);
+    
+     updateTable($db,'tblhealthcare',$healthCare,$where);
+}
+
+if(isset($_POST["modalid"]) && isset($_POST['action']) && $_POST['action'] == "eventupdate")
+{
+    $startdate = $_POST['modalstart'];
+    $enddate = $_POST['modalend'];
+    $updateArr = array(
+                'title'  => $_POST['modaltitle'],
+                'start_event' => $startdate,
+                'end_event' => $enddate,
+                'salesrepid' => $_POST['modalsalesrepId'],
+                'accountid' => $_POST['modalaccountId'],  
+               );
+    $where = array('id' => $_POST['modalid']);
+    
+    updateTable($db,'tblevents',$updateArr,$where);
+
+    /* ------- Update Comment ------- */
+
+    if($_POST['commentid']){
+        $updateArrComments = array(
+                            'comments' => $_POST['modalcomments'],
+                            'eventid' => $_POST['modalid'],
+                            'user_id' => $_POST['userid'],
+                            'updated_date' => date("Y-m-d H:i:s"),
+                        );
+        $where = array('id' => $_POST['commentid']);
+        updateTable($db, 'tblcomments', $updateArrComments, $where );
+    }else{
+        $addArrComments = array(
+                            'comments' => $_POST['modalcomments'],
+                            'eventid' => $_POST['modalid'],
+                            'user_id' => $_POST['userid'],
+                            'created_date' => date("Y-m-d H:i:s"),
+                            'updated_date' => date("Y-m-d H:i:s"),
+                        );
+        insertIntoTable($db, 'tblcomments', $addArrComments);
+    }
+    
+}
+
+/* --------------------- Get Comment ------------------------- */
+
+if(isset($_POST['eventid']) && isset($_POST['action']) && $_POST['action'] == "getComment"){
+    $eventId = $_POST['eventid'];
+
+    $query = "SELECT  rep.first_name as repfname, rep.last_name as replname, admin.first_name as adminfname, admin.last_name as adminlname, com.* "
+            ."FROM `tblcomments` com "
+            ."LEFT JOIN `tbladmins` admin ON com.user_id = admin.Guid_user "
+            ."LEFT JOIN `tblsalesrep` rep ON com.user_id = rep.Guid_user "
+            ."WHERE com.eventid = ".$eventId." ORDER BY `id` DESC";
+    $result = $db->query($query);
+    
+    echo json_encode($result);
+}
+
+/* --------------------- Delete Comment ------------------------- */
+
+if(isset($_POST['commentid']) && isset($_POST['action']) && $_POST['action'] == "commentDelete"){
+    $commentid = $_POST['commentid'];
+    $query = "DELETE FROM `tblcomments` WHERE id=".$commentid;
+    $result = $db->query($query);
+    echo json_encode($result);
+}
---
+++ login/functions-ss-login.php
@@ -0,0 +1,168 @@
+<?php
+
+function sec_session_start() {
+    $session_name = SESSION_NAME;   // Set a custom session name 
+    $secure = SECURE;
+    // This stops JavaScript being able to access the session id.
+    $httponly = true;
+    // Forces sessions to only use cookies.
+    if (ini_set('session.use_only_cookies', 1) === FALSE) {
+        header("Location: error.php?err=Could not initiate a safe session (ini_set)");
+        exit();
+    }
+    // Gets current cookies params.
+    $cookieParams = session_get_cookie_params();
+    session_set_cookie_params($cookieParams["lifetime"], $cookieParams["path"], $cookieParams["domain"], $secure, $httponly);
+    // Sets the session name to the one set above.
+    session_name($session_name);
+    if(!isset($_SESSION)){ 
+        session_start();  // Start the PHP session  
+    }    
+    session_regenerate_id();    // regenerated the session, delete the old one. 
+}
+function login($email, $password, $db) {
+    $db->bind("email",$email);
+    $user = $db->row("SELECT * FROM `".DB_PREFIX."user` WHERE email = :email"); 
+    if ($user) { 
+        if (checkbrute($user['email'], $db) == true) {
+            // Account is locked 
+            // Send an email to user saying their account is locked 
+            $showMsg = false;
+            $message = "Your account has been locked for 2 hours due to the multiple failed login attempts. Please try again later. If you need immediate assistance, please contact your support team.";
+            $returnData = array('status'=>false, $showMsg=>false, 'message'=>$message);
+            return $returnData;
+        } else {            
+            // Check if the password in the database matches 
+            // the password the user submitted.
+            if ($user["password"] == encode_password($password)) {
+                // Password is correct!
+                // Get the user-agent string of the user.
+                $user_browser = $_SERVER['HTTP_USER_AGENT'];
+                // XSS protection as we might print this value
+                $Guid_user = preg_replace("/[^0-9]+/", "", $user['Guid_user']);
+                $_SESSION['user']['id'] = $Guid_user;
+                // XSS protection as we might print this value                
+                $_SESSION['user']['type'] = $user['user_type'];
+                $_SESSION['user']['email'] = $user['email'];
+                $_SESSION['login_string'] = hash('sha512', encode_password($password) . PASSWORD_SALT . $user_browser);
+                // Login successful. 
+                $returnData = array('status'=>true);
+                return $returnData;
+            } else {
+                // Password is not correct 
+                // We record this attempt in the database 
+                $showMsg = true;
+                $now = time();                
+                $ipAddr = get_client_ip();                
+                if (!$db->query("INSERT INTO tbluser_login_attempts (email, ip, time) VALUES ('$email', '$ipAddr', '$now')")) {
+                    header("Location: ../error.php?err=Database error: tbluser_login_attempts");
+                    exit();
+                }
+                $message = "Username or password is not correct.";
+                $returnData = array('status'=>false, $showMsg=>true, 'message'=>$message);
+                return $returnData;
+            }
+        }
+       
+    } else {
+        // Could not create a prepared statement
+        header("Location: ../error.php?err=Database error: cannot prepare statement");
+        exit();
+    }
+}
+function checkbrute($email, $db) {
+    // Get timestamp of current time 
+    $now = time();
+    // All login attempts are counted from the past 2 hours. 
+    $valid_attempts = $now - (2 * 60 * 60);
+    $db->bind("email",$email);
+    $stmt = $db->query("SELECT time FROM tbluser_login_attempts WHERE email = :email AND time > '$valid_attempts'");
+  
+    if (!empty($stmt)) { 
+        if (count($stmt) > 5) { // If there have been more than 5 failed logins 
+            return true;
+        } else {
+            return false;
+        }
+    } 
+    return false;
+}
+function login_check($db) {
+    // Check if all session variables are set 
+    if (isset($_SESSION['user']['id'], $_SESSION['user']['email'], $_SESSION['login_string'])) {
+        $Guid_user = $_SESSION['user']['id'];
+        $login_string = $_SESSION['login_string'];
+        $username = $_SESSION['user']['email'];
+        // Get the user-agent string of the user.
+        $user_browser = $_SERVER['HTTP_USER_AGENT'];
+        $db->bindMore( array('Guid_user'=>$Guid_user, 'status'=>'1') );
+        $stmt = $db->row("SELECT * FROM tbluser WHERE Guid_user=:Guid_user AND status=:status LIMIT 1");
+        
+        if (!empty($stmt)) {
+            // If the user exists get variables from result.
+            $password = $stmt['password'];
+            $login_check = hash('sha512', $password . PASSWORD_SALT . $user_browser);
+            if ($login_check == $login_string) {// Logged In!!!! 
+                return true;
+            } else { // Not logged in 
+                return false;
+            }
+        } else { // Not logged in 
+            return false;
+        }        
+    } else { // Not logged in 
+        return false;
+    }
+}
+// Function to get the client IP address
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+function esc_url($url) {
+    if ('' == $url) {
+        return $url;
+    }
+    $url = preg_replace('|[^a-z0-9-~+_.?#=!&;,/:%@$\|*\'()\\x80-\\xff]|i', '', $url);    
+    $strip = array('%0d', '%0a', '%0D', '%0A');
+    $url = (string) $url;   
+    $count = 1;
+    while ($count) {
+        $url = str_replace($strip, '', $url, $count);
+    }    
+    $url = str_replace(';//', '://', $url);
+    $url = htmlentities($url);    
+    $url = str_replace('&amp;', '&#038;', $url);
+    $url = str_replace("'", '&#039;', $url);
+    if ($url[0] !== '/') {
+        // We're only interested in relative links from $_SERVER['PHP_SELF']
+        return '';
+    } else {
+        return $url;
+    }
+}
+
+function logout(){
+    // Unset all session values 
+    $_SESSION = array();
+    // get session parameters 
+    $params = session_get_cookie_params();
+    // Delete the actual cookie. 
+    setcookie(session_name(),'', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+    // Destroy session 
+    session_destroy();
+}
