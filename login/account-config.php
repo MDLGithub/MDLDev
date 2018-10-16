dd02eec0b856ea80c773cdad999fc042a52eb96c
vijaykumarsatyamsetti <vijaykumar.satyamsetti@gmail.com>
2018-10-16 19:10:35 +0530

    file updated

--- login/account-config.php
+++ login/account-config.php
@@ -458,6 +458,9 @@ require_once ('navbar.php');
                         <a class="add-new-device" href="?action=add">
                             <span class="fas fa-plus-circle" aria-hidden="true"></span> Add
                         </a>
+                        <a class="followup" id="followup" href="javascript:void(0)" onclick="PrintPreview();" target="_blank">
+                            <span class="fas fa-print" aria-hidden="true"></span> Follow Up
+                        </a>
                     </div>
                     <?php } ?>
                 </div>
@@ -553,7 +556,514 @@ require_once ('navbar.php');
      </div>
     <?php } ?>
 </main>
+
+<section class="form follow-up geneveda" id="printarea">
+    <style type="text/css" >
+        @charset "utf-8";
+        @media print {
+            .exact, .form * {-webkit-print-color-adjust: exact;print-color-adjust: exact;}
+            .c2 th:nth-of-type(2), .c2 td:nth-of-type(2), .c3 th:nth-of-type(3), .c3 td:nth-of-type(3),
+            .c4 th:nth-of-type(4), .c4 td:nth-of-type(4), .c5 th:nth-of-type(5), .c5 td:nth-of-type(5) {
+                text-align: center;}
+            .sf4 td:nth-of-type(4) {font-size: 2.47mm;font-weight: normal;max-width: 50mm;}
+            .columns {display: flex;}
+            .columns > .col {flex-grow:1;}
+            .columns.two > .col {width: 50%;}
+            .dashed {border: .42 dashed #000; border-top:0; border-bottom:0;}
+            .geneveda .dashed {border-color: #7a68ae;}
+            .follow-up p {font-size: 3.53mm;line-height: 5.29mm;margin-top: 7.95mm;color: #242424 !important;}
+            .follow-up .account_logo {max-width: 54.18mm;}
+            .follow-up .lab {text-align: right;margin-bottom: 10.16mm;}
+            .follow-up .lab > img {max-width: 44.36mm;display: inline-block;}
+            .follow-up .header {display: flex; padding-bottom: 7.19mm;}
+            .follow-up .header .col.one { width: 76.2mm;}
+            .follow-up .header .col.two {flex-grow: 1;text-align: right; }
+            .follow-up .main {border: .67mm solid #cbcbcb; border-left:0; border-right: 0;padding-bottom: 7.62mm;}
+            .form p > .labColor, .lC5 td:nth-of-type(5) {color: #7a68ae !important;
+                -webkit-print-color-adjust: exact;
+                print-color-adjust: exact;
+            }
+            
+            .geneveda .ftable > th {
+                background: #7a68ae;
+            }
+            
+            .pBG_size {
+                position: relative;
+            }
+            
+            .pBG {
+                width: 100%;
+                height:100%;
+                position: absolute;
+                left:0;
+                top:0;
+                z-index:-1;
+            }
+            
+            .ftable {
+                position: relative;
+                width: 100%;
+                font-size: 2.82mm;
+                margin-top: 4.23mm;
+                font-weight: 600;
+                text-align: left;
+            }
+            
+            .ftable th, .ftable .pBG {
+                height: 5.24mm;
+            }
+            
+            .ftable th {
+                color: #fff !important;
+                -webkit-print-color-adjust: exact;
+                print-color-adjust: exact;
+                vertical-align: middle;
+            }
+            
+            .ftable tr {
+                min-height: 5.24mm;
+            }
+            
+            .ftable td {
+                border-bottom: .16mm solid #4c4c4c;
+                padding: 1.35mm 0;
+                color: #2b2b2b !important;
+            }
+            
+            .ftable th:first-child, .ftable td:first-child {
+                padding-left: 4.23mm;
+            }
+            
+            #testing_recommended th:last-child, #testing_recommended td:last-child {
+                text-align: center;
+                width: 28.61mm;
+            }
+            
+            #testing_recommended td:last-child {
+                border-left: .42mm dashed #7a68ae; border-right: .42mm dashed #7a68ae;
+                text-align: center;
+            }
+            
+            .sTable {
+                text-align: left;
+                width: 100%;
+                table-layout: fixed;
+                margin-top: 3.81mm;
+            }
+            
+            .sTable td, .sTable th  {
+                border: .16mm solid #4c4c4c; border-top:0; border-left:0;
+                width: 100%;
+                font-size: 2.82mm;
+                padding-left: 3.13mm;
+            }
+            
+            .sTable td:last-child, .sTable th:last-child {
+                border-right:0; 
+            }
+            
+            .sTable th {
+                font-weight: bold;
+                padding-top: 2.03mm; padding-bottom: 2.03mm;
+            }
+            
+            .geneveda .sTable th {
+                color: #7a68ae !important;
+                -webkit-print-color-adjust: exact;
+                print-color-adjust: exact;
+            }
+            
+            .sTable td {
+                font-weight: 600;
+                color: #2b2b2b !important;
+                max-width: 33%;
+                padding-top: 1.69mm; padding-bottom: 1.69mm;
+            }
+            
+            .sTable td:first-child, .sTable th:first-child {
+                max-width: 28.36mm;
+            }
+            
+            #not_completed {
+                padding-left: 6.77mm;
+            }
+            
+            #not_completed .sTable {
+                margin-top: 9mm;
+            }
+            
+            #not_recommended .sTable {
+                width: calc(100% - 7.62mm);
+            }
+            
+            #not_completed th:last-child, #not_recommended th:last-child {
+                max-width: 21mm;
+            }
+            
+            .form footer {
+                padding-top: 6.94mm;
+                font-size: 8pt;
+            }
+            
+            .gc_title {
+                padding-bottom: 5.41mm;
+            }
+            
+            .gc_title > h4 {
+                font-weight: bold;
+                font-size: 10pt;
+                padding-bottom: 1.27mm;
+            }
+            
+            .gc_title > em {
+                font-size: 8pt;
+                color: #242424 !important;
+            }
+            
+            .columns .foogo {
+                flex-grow: 0;
+                padding-right: 4.23mm;
+            }
+            
+            .foogo > img {
+                width: 13.88mm;
+            }
+            
+            .gc_contact {
+                border-top: 0.25mm solid #1c1c1c;
+                flex-grow: 1;
+                display: flex;
+                margin-top: 2.11mm;
+                position: relative;
+                padding-top: 1.86mm;
+            }
+            
+            .gc_contact > section {
+                flex-grow: 1;
+                padding-left: 3.81mm;
+            }
+            
+            .gc_contact::before {
+                content:"";
+                position: absolute;
+                left:-3.72mm; top:-.2mm;
+                z-index:1;
+                font-size: inherit;
+                display: inline-block;
+                width: 3.72mm;
+                height: .25mm;
+                border-top: 0.25mm solid #1c1c1c;
+                transform-origin: bottom right;
+                transform: rotate(45deg);
+            }
+            
+            .gc_contact::after, .gc_contact > section::after {
+                content:"";
+                position: absolute;
+                border-radius: 50%;
+                width: 0.84mm;
+                height: 0.84mm;
+                border: .33mm solid #1c1c1c;
+                overflow: hidden;
+                right:0;
+                top: -.5mm;
+            }
+            
+            .gc_contact > section::after {
+                right: auto;
+                left:-2.8mm;
+                top:-2.8mm;
+            }
+            
+            .gc_contact h5 {
+                text-transform: uppercase;
+                font-weight: 600;
+            }
+            
+            .gc_contact span {
+                padding-left: 3.38mm;
+            }
+            
+            .gc_contact > * {
+                line-height: 11pt;
+            }
+            
+            .gc_contact li {
+                position: relative;
+            }
+            
+            .gc_contact > ul:last-child {
+                margin-left: 12.63mm;
+                font-weight: 600;
+            }
+            
+            .gc_contact img {
+                position: absolute;
+                max-width: 2.62mm;
+                max-height: 2.62mm;
+                left: -4mm;
+                top: .9mm;
+            }
+            
+            .gc_contact .gc_email {
+                left:-4.8mm;
+            }
+            
+            .sb {
+                font-weight: 600;
+            }
+            
+            .stat_table {
+                text-align: left;
+                position: relative;
+                font-size: 8pt;
+                font-weight: 600;
+                color: #242424 !important;
+            }
+            
+            .follow-up .stat_table {
+                display: inline-block;
+            }
+            
+            .stat_table th {
+                border-right: .42mm solid #000;
+                padding: 1.69mm 2.03mm;
+            }
+            
+            .stat_table td {
+                border-left: .42mm solid #000;
+                border-bottom: .25mm solid #000;
+                padding: 2.03mm 0;
+            }
+            
+            .stat_table td:first-child {
+                padding-right: 3.55mm; padding-left: 3.55mm;
+            }
+            
+            .stat_table tr:last-child > td {
+                border-bottom: 0;
+            }
+            
+            .stat_table td:last-child {
+                border-right: .42mm solid #000;
+                color: #7a68ae !important;
+            }
+            
+            .geneveda .stat_table th, .geneveda .stat_table td {
+                border-color: #7a68ae;
+            }
+            
+            .side_head {
+                position:absolute !important;
+                left:-12mm;
+                transform: rotate(-90deg);
+                bottom: 12mm;
+                text-transform: uppercase;
+                color: #9e9e9e !important;
+                font-weight: bold;
+                letter-spacing: .25mm;
+            }
+            
+            .follow-up .stat_table .pBG {
+                width: 12mm;
+                height: 25.4mm;
+                top: auto;
+                left: auto;
+                bottom:0;
+                right: 11.5mm;
+            }
+        }
+    </style>
+    <input type="button" value="Print" class="btn" onclick="window.print();"/>
+    <header class="header">
+        <div class="col one">
+            <figure class="account_logo"><img src="https://www.mdlab.com/dev/images/practice/22230-LEXINGTON%20OBGYN%20ASSOCIATES.png" alt="Lexington"></figure>
+
+            <p>Thank you for selecting Geneveda to provide Hereditary Breast and Ovarian Cancer (HBOC) Screening for your patients. This report was prepared on <strong>September 24, 2018</strong>.</p>
+        </div>
+    
+        <div class="col two">
+            <figure class="lab"><img src="https://www.mdlab.com/dev/images/logo_geneveda.png" alt="Geneveda"></figure>
+            
+            <table class="stat_table c2 c3">
+                <thead>
+                    <tr>
+                        <th><h4 class="side_head">Summary</h4></th>
+                        <th><img class="pBG" src="images/swatch_gen_green.png" alt="">Today</th>
+                        <th>Total</th>
+                    </tr>
+                </thead>
+                
+                <tbody>
+                    <tr>
+                        <td>Registered Patients</td>
+                        <td>11</td>
+                        <td>24</td>
+                    </tr>
+                    <tr>
+                        <td>Completed Questionnaire</td>
+                        <td>9</td>
+                        <td>20</td>
+                    </tr>
+                    <tr>
+                        <td>Insufficient Information</td>
+                        <td>2</td>
+                        <td>5</td>
+                    </tr>
+                    <tr>
+                        <td>Medically Qualified</td>
+                        <td>4</td>
+                        <td>12</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </header>
+    
+    <div class="main">
+        <p>Based upon the cited clinical policy testing guidelines, testing is <strong class="labColor">recommended</strong> for the following patients:</p>
+        
+        <table id="testing_recommended" class="ftable c4 c5 c6 lC5">
+            <thead>
+                <tr>
+                    <th><img class="pBG" src="images/swatch_gen_purple.png" alt="">First Name</th>
+                    <th>Last Name</th>
+                    <th>DOB</th>
+                    <th>Guideline Applied</th>
+                    <th>Guideline Met</th>
+                    <th>Date Collected</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>1/1/1980</td>
+                    <td>Aetna</td>
+                    <td>BRCA</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>1/1/1980</td>
+                    <td>Aetna</td>
+                    <td>BRCA</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        
+        <p><strong>Insufficient information</strong> was available to determine if the following patients met clinical policy testing guidelines:</p>
+        
+        <table class="ftable sf4">
+            <thead>
+                <tr>
+                    <th><img class="pBG" src="images/swatch_gen_grey.png" alt="">First Name</th>
+                    <th>Last Name</th>
+                    <th>DOB</th>
+                    <th>Information Needed</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>1/1/1980</td>
+                    <td>Genetic Mutation</td>
+                </tr>
+                <tr>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>1/1/1980</td>
+                    <td>Do you have at least one first- or second-degree close blood relative in your family with breast cancer at age 45 years or younger?</td>
+                </tr>
+            </tbody>
+        </table>
+        
+        <div id="not_recommended" class="columns two">
+            <div class="col">
+                <p>The following patients were also screened and based upon the information provided, screening for BRCA, HBOC, and/or Lynch Syndrome is <strong>not</strong> recommended:</p>
+            
+                <table class="sTable">
+                    <thead>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>DOB</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Jane</td>
+                            <td>Smith</td>
+                            <td>3/3/1985</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            
+            <div id="not_completed" class="col">
+                <p>The following patients initiated, but did not complete the questionnaire:</p>
+            
+                <table class="sTable">
+                    <thead>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>DOB</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Irma</td>
+                            <td>Redulus</td>
+                            <td>9/18/1965</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    
+    <p>Access your physician portal any time at <span class="labColor">https://www.mdlab.com/questionnaire/login</span>. Please contact me for any additional details or if you do not have access to your portal.</p>
+    
+    <footer>
+        <section class="gc_title">
+            <h4>Brandon Franklin, PhD</h4>
+            <em>Specialty Sales Consultant, Division of Genetics and Oncology</em>
+        </section>
+        
+        <div class="columns">
+            <figure class="foogo col"><img src="images/logo_mdl_print.svg" alt=""></figure>
+
+            <div class="gc_contact col">
+                <section>
+                    <h5>Medical Diagnostic Laboratories, L.L.C.</h5>
+                    <span>www.<b class="sb">mdlab</b>.com</span>
+                </section>
+
+                <ul>
+                    <li><img src="images/icon_address.svg" alt="">2439 Kuser Rd</li>
+                    <li>Hamilton, NJ 08690</li>
+                </ul>
+
+                <ul>
+                    <li><img src="images/icon_phone.svg" alt="">888.414.3237</li>
+                    <li><img class="gc_email" src="images/icon_email.svg" alt="">bfranklin@mdlab.com</li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</section>
+
+
 <?php require_once('scripts.php');?>
+
+<script src='assets/js/jspdf.debug.js'></script>
+<script src='assets/js/html2pdf.js'></script> 
+
 <script type="text/javascript">  
     if ($('#dataTable').length ) {
         var table = $('#dataTable').DataTable({
@@ -564,7 +1074,22 @@ require_once ('navbar.php');
             "order": [[ 1, "asc" ]]
         });   
     }
-    
-     
+    function PrintPreview() {
+
+        var toPrint = document.getElementById('printarea');
+
+        var popupWin = window.open('', '_blank','width=900,height=900,location=no,left=200px');
+
+        popupWin.document.open();
+
+        popupWin.document.write('<html><title>::Print Preview::</title><link rel="stylesheet" type="text/css" href="assets/css/forms.css" media="screen"/></head><body">');
+
+        popupWin.document.write(toPrint.innerHTML);
+
+        popupWin.document.write('</html>');
+
+        popupWin.document.close();
+
+    }
 </script>
 <?php require_once('footer.php');?>
