53808936683348356863d4c6b3d1673e3d6c7c44
gabrielajohnsonmdl <44236298+gabrielajohnsonmdl@users.noreply.github.com>
2018-10-23 16:25:10 -0400

    BRCA Forms JS Modal script

--- login/assets/js/brca_forms.js
+++ login/assets/js/brca_forms.js
@@ -22,15 +22,15 @@ $(document).ready(function(){
    activeItem = $("#accordion li:first");
    $(activeItem).addClass('active');
 
-   /*$("#accordion #form-bar").click(function(){
+   $("#accordion #form-bar").click(function(){
        $(activeItem).animate({width: "50px"}, {duration:300, queue:false});
        $(this).parent().animate({width: "80%"}, {duration:300, queue:false});
        activeItem = $(this).parent();
-   });*/
+   });
 
     $(".next-button").click(function(){
       $(this).parent().parent().parent().animate({width: "50px"}, {duration:300, queue:false});
-      $(activeItem).next().animate({width: "75%"}, {duration:300, queue:false});
+      $(activeItem).next().animate({width: "80%"}, {duration:300, queue:false});
       activeItem = $(activeItem).next();
     });
 
@@ -39,10 +39,30 @@ $(document).ready(function(){
       /*$(activeItem).animate({width: "50px"}, {duration:300, queue:false});
       $(activeItem).prev().animate({width: "80%"}, {duration:300, queue:false});*/
       $(this).parent().parent().parent().animate({width: "50px"}, {duration:300, queue:false});
-      $(activeItem).prev().animate({width: "75%"}, {duration:300, queue:false});
+      $(activeItem).prev().animate({width: "80%"}, {duration:300, queue:false});
       activeItem = $(activeItem).prev();
     });
- 
 
+        // Get the modal
+        var modal = document.getElementById('myModal');
+
+        $(".patient_forms").click(function(){
+            modal.style.display = "block";
+        });
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
 
 });
